610908f466fd64de675b747f0a47a05fd752887e
Younsu Han <younsuhan@Younsuui-MacBookPro.local>
2024-08-21 12:59:47 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>{{ message }}</h1>
+    <p v-if="isVisable">
+      This paragraph is conditionally rendered.
+    </p>
+    <ul>
+      <li v-for="item in items" :key="item">{{ item }}</li>
+    </ul>
+    <MyComponent title="My Custom Component" @message="handleMessage"></MyComponent>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyComponent from './components/MyComponent.vue';
 
 export default {
-  name: 'App',
+  data() {
+    return {
+      message: 'Hello, Vue.js!',
+      isVisable : true,
+      items : ['Item 1', 'Item 2', 'Item 3']
+    };
+  },
   components: {
-    HelloWorld
+    MyComponent
+  },
+  methods: {
+    handleMessage(payload){
+      alert(payload);
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
---
+++ src/components/MyComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h2>{{ title }}</h2>
+        <button @click="sendMessage">Click me</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['title'],
+        methods: {
+            sendMessage() {
+                this.$emit('message', 'Hello from MyComponent')
+            }
+        }
+    };
+</script>
